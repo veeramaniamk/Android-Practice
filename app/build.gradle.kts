b0399d1a8c97f3c441c32544c8e9655f1de99053
veera <veeramaniamk@gmail.com>
2026-08-18 21:57:40 +0530

    uneccessary build folders are removed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,13 +51,13 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-scalars:2.11.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
-    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+//    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+//    implementation("com.squareup.retrofit2:converter-scalars:2.11.0")
+//    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+//    implementation("com.squareup.okhttp3:okhttp:4.9.3")
 
-    implementation("com.jsibbold:zoomage:1.3.1")
-
-    implementation("io.github.afreakyelf:Pdf-Viewer:2.3.7")
+//    implementation("com.jsibbold:zoomage:1.3.1")
+//
+//    implementation("io.github.afreakyelf:Pdf-Viewer:2.3.7")
 
 }
