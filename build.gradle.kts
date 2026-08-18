42c28f1f371ffd3f5c2ce29b84b0cef3cadf8185
veera <veeramaniamk@gmail.com>
2026-08-18 23:24:33 +0530

    just module created

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+buildscript {
+    val dd by extra("")
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
--- usb-adapter/build.gradle.kts
+++ usb-adapter/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
-    namespace = "com.saveetha.usb-adapter"
+    namespace = "com.saveetha.usb_adapter"
     compileSdk = 35
 
     defaultConfig {
@@ -30,6 +31,9 @@ android {
         jvmTarget = "11"
     }
 
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
@@ -37,9 +41,20 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 
 //    implementation("com.squareup.retrofit2:retrofit:2.11.0")
 //    implementation("com.squareup.retrofit2:converter-scalars:2.11.0")
