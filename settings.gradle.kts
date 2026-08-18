d6358788fcbe40d940f0fd101ca687b5f2881c97
veera <veeramaniamk@gmail.com>
2026-08-18 23:00:27 +0530

    build configuration loaded and usb-adapter module also created

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Practice"
 include(":app")
+include(":usb-adapter")
---
+++ usb-adapter/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.saveetha.usb-adapter"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+//    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+//    implementation("com.squareup.retrofit2:converter-scalars:2.11.0")
+//    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+//    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+
+//    implementation("com.jsibbold:zoomage:1.3.1")
+//
+//    implementation("io.github.afreakyelf:Pdf-Viewer:2.3.7")
+
+}
